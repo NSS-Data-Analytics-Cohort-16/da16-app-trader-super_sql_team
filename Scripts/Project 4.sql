bae1a9ade4515dbe165c585f2436ae26513880b5
ansonrk <ansonrk@gmail.com>
2025-10-16 21:19:55 -0500

    proj 4

--- Scripts/Project 4.sql
+++ Scripts/Project 4.sql
@@ -7,7 +7,7 @@ from app_store_apps
 
 SELECT app_store_apps.name,
   MAX(price) AS max_price,
-  10000.0 * GREATEST(1.0, MAX(price)) AS purchase_price
+  10000.0 * GREATEST(1.0, MAX(price)):: money AS purchase_price
 FROM app_store_apps
 GROUP BY app_store_apps.name
 ORDER BY purchase_price DESC;
@@ -18,3 +18,24 @@ SELECT play_store_apps.name,
 FROM play_store_apps
 GROUP BY play_store_apps.name
 ORDER BY max_price
+
+Select Play_store_apps.genres as pl_genres, 
+	   review_count as rv_count 
+FROM Play_store_apps
+group by pl_genres,
+          rv_count
+order by rv_count desc
+
+
+select price, rating, review_count, name
+from app_store_apps
+order by rating desc
+
+
+
+
+
+
+
+
+
